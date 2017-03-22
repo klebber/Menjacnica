f8ed9e4489029d2fbc94ee20ad9c339aeef87f3a
klebber <bstojev199@gmail.com>
2017-03-22 21:29:45 +0100

    Uvedene hashCode, equals i toString metode.

--- Menjacnica/src/menjacnica/Kurs.java
+++ Menjacnica/src/menjacnica/Kurs.java
@@ -19,5 +19,37 @@ public class Kurs {
 	public void setDatum(SimpleDateFormat datum) {
 		this.datum = datum;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((datum == null) ? 0 : datum.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(vrednostKursa);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Kurs other = (Kurs) obj;
+		if (datum == null) {
+			if (other.datum != null)
+				return false;
+		} else if (!datum.equals(other.datum))
+			return false;
+		if (Double.doubleToLongBits(vrednostKursa) != Double.doubleToLongBits(other.vrednostKursa))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Kurs [Vrednost kursa: " + vrednostKursa + ", datum: " + datum + "]";
+	}
 	
 }
--- Menjacnica/src/menjacnica/Valuta.java
+++ Menjacnica/src/menjacnica/Valuta.java
@@ -26,5 +26,44 @@ public class Valuta {
 	public void setKursevi(LinkedList<Kurs> kursevi) {
 		this.kursevi = kursevi;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((kursevi == null) ? 0 : kursevi.hashCode());
+		result = prime * result + ((nazivValute == null) ? 0 : nazivValute.hashCode());
+		result = prime * result + ((skracenica == null) ? 0 : skracenica.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Valuta other = (Valuta) obj;
+		if (kursevi == null) {
+			if (other.kursevi != null)
+				return false;
+		} else if (!kursevi.equals(other.kursevi))
+			return false;
+		if (nazivValute == null) {
+			if (other.nazivValute != null)
+				return false;
+		} else if (!nazivValute.equals(other.nazivValute))
+			return false;
+		if (skracenica == null) {
+			if (other.skracenica != null)
+				return false;
+		} else if (!skracenica.equals(other.skracenica))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Valuta [Naziv valute: " + nazivValute + ", skracenica: " + skracenica + ", kursevi: " + kursevi + "]";
+	}
 
 }
