849a03310b4f5735328f7911d60181faf7ef01d8
klebber <bstojev199@gmail.com>
2017-03-22 21:40:35 +0100

    Napravljena nova klasa Menjacnica koja implementira interfejs MenjacnicaInterfejs.

---
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -0,0 +1,27 @@
+package menjacnica;
+
+import java.text.SimpleDateFormat;
+
+import interfejs.MenjacnicaInterfejs;
+
+public class Menjacnica implements MenjacnicaInterfejs {
+
+	@Override
+	public void dodavanjeKursa(String nazivValute, SimpleDateFormat datum) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void brisanjeKursa(String nazivValute, SimpleDateFormat datum) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Kurs pretragaKursa(String nazivValute, SimpleDateFormat datum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
